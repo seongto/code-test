951862b71551e457b280321553ad60b29a43b13e
Seongto <imseongto@gmail.com>
2024-11-07 09:22:54 +0900

    [level 1] Title: 직사각형 별찍기, Time: 6.43 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.swift
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.swift
@@ -3,6 +3,9 @@ import Foundation
 let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
 let (a, b) = (n[0], n[1])
 
-for num in 1...b {
-    print(String(repeating: "*", count: a))
-}
+// for num in 1...b {
+//     print(String(repeating: "*", count: a))
+// }
+
+// 아래 코드는 분명 맨 아래에 빈줄이 하나 추가되어 사실 오답으로 봐야하지만 통과가 된다..?
+print(String(repeating: "\(String(repeating: "*", count: a))\n", count: b))

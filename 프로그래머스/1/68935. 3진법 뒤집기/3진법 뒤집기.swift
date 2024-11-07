ff0a1f78cf4a6ae19de3b923d53fa5bd6968a053
Seongto <imseongto@gmail.com>
2024-11-07 10:23:57 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.09 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
@@ -1,16 +1,7 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    let binary:String = String(n ,radix: 3)
-    var numArr: [String] = binary.map { String($0) }.reversed()
+    let binary:String = String(n ,radix: 3)            
     
-    var answer: Int = 0
-        
-    for ( idx, num ) in numArr.enumerated() {
-        if num != "0" {
-            answer += Int(num)! * Int(NSDecimalNumber(decimal: pow(3, numArr.count - (idx + 1))))
-        }
-    }
-    
-    return answer
+    return Int(String(binary.reversed()), radix: 3)!
 }

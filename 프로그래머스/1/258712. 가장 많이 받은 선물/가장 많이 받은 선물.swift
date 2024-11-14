45dd02e0bc80ddc392588aa708a19b7123db72ae
Seongto <imseongto@gmail.com>
2024-11-15 00:43:47 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 3075.84 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.swift
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.swift
@@ -1,59 +1,47 @@
 import Foundation
 
 func solution(_ friends:[String], _ gifts:[String]) -> Int {
-    var giftIndex: [String:Int] = [:]
-    var giftToGet: [String:Int] = [:]
-    
-    // 선물지수 비교하여 선물 지급하기
-    func indexCheck(_ name1: String, _ name2: String) -> Void {
-        if giftIndex[name1]! > giftIndex[name2]! {
-            giftToGet[name1]! += 1
-        } else if giftIndex[name1]! < giftIndex[name2]! {
-            giftToGet[name2]! += 1
-        }
+    var giftIndex: [String: Int] = [:]
+    var giftToGet: [String: Int] = [:]
+    var giftMap: [String: Int] = [:] // 선물 관계를 빠르게 조회하기 위한 딕셔너리
+
+    // 1. 선물 관계를 딕셔너리에 저장
+    for gift in gifts {
+        giftMap[gift, default: 0] += 1
     }
-    
-    // 1. 개인의 선물지수 데이터 저장
+
+    // 2. 개인의 선물 지수 계산
     for name in friends {
-        var plusIndex: Int = gifts.reduce(0) { $1.split(separator: " ")[0] == name ? $0 + 1 : $0 + 0}
-        var minusIndex: Int = gifts.reduce(0) { $1.split(separator: " ")[1] == name ? $0 + 1 : $0 + 0}
-        giftIndex[ name ] = plusIndex - minusIndex
-        giftToGet[ name ] = 0
+        let plusIndex = gifts.filter { $0.split(separator: " ")[0] == name }.count
+        let minusIndex = gifts.filter { $0.split(separator: " ")[1] == name }.count
+        giftIndex[name] = plusIndex - minusIndex
+        giftToGet[name] = 0
     }
-    print(giftIndex)
-    
-    // 2. 개인별 순회하며 다른 멤버와의 선물 관계성 계산
-    for idx1 in 0..<friends.count-1 {
+
+    // 3. 선물 관계를 계산하여 다음 달 받을 선물 계산
+    for idx1 in 0..<friends.count - 1 {
         for idx2 in idx1 + 1..<friends.count {
-            let name1: String = friends[idx1]
-            let name2: String = friends[idx2]
-            let gift1to2: String = name1 + " " + name2
-            let gift2to1: String = name2 + " " + name1
-            
-            if  gifts.contains(gift1to2) || gifts.contains(gift2to1) {
-                var sum: Int = gifts.reduce(0) { 
-                    if $1 == gift1to2 {
-                        return $0 + 1
-                    } else if $1 == gift2to1 {
-                        return $0 - 1
-                    }
-                    return $0
-                }
-                
-                // 3. 선물 관계에 따른 다음달 받을 선물 계산하여 저장
-                if sum > 0 {
-                    giftToGet[name1]! += 1
-                } else if sum < 0 {
-                    giftToGet[name2]! += 1
-                } else {
-                    indexCheck(name1, name2)
-                }
-                    
+            let name1 = friends[idx1]
+            let name2 = friends[idx2]
+            let gift1to2 = "\(name1) \(name2)"
+            let gift2to1 = "\(name2) \(name1)"
+
+            // 선물 관계 점수 계산
+            let sum = (giftMap[gift1to2] ?? 0) - (giftMap[gift2to1] ?? 0)
+
+            if sum > 0 {
+                giftToGet[name1]! += 1
+            } else if sum < 0 {
+                giftToGet[name2]! += 1
             } else {
-                indexCheck(name1, name2)
+                if giftIndex[name1]! > giftIndex[name2]! {
+                    giftToGet[name1]! += 1
+                } else if giftIndex[name1]! < giftIndex[name2]! {
+                    giftToGet[name2]! += 1
+                }
             }
         }
     }
-    
+
     return giftToGet.values.max()!
 }

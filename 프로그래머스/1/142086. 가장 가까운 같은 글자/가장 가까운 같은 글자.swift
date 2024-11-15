b8cf35b8c7febc1f7d0883e49b7be1b33a77b6dc
Seongto <imseongto@gmail.com>
2024-11-15 11:06:28 +0900

    Update 가장 가까운 같은 글자.swift

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.swift
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.swift
@@ -1,47 +1,23 @@
 import Foundation
 
 func solution(_ s:String) -> [Int] {
-    var answer: [Int] = []
-    var strDic: [String : Int] = [:]
-    
-    var strs: [String] = s.map { 
-        strDic[String($0), default: 0] += 1
-        return String($0) 
-    }
+    var answer: [Int] = []   
+    var strDic: [String : Int] = [:] // 마지막 인덱스 저장
+    var strs: [String] = s.map { String($0) }
     
     for (idx, str) in strs.enumerated() {
-        if ( idx == 0) || ( strDic[str] == 1 ) {
+        if idx == 0 {
             answer.append( -1 )
         } else {
-            if let i = Array(strs[0...idx - 1]).lastIndex(of: str) {
+            if let i = strDic[str] {
                 answer.append(idx - i)
             } else {
                 answer.append(-1)
             }
         }
+        // 해당 글자의 마지막 인덱스를 저장.
+        strDic[str] = idx
     }
-    
-    
-    
-//     for ( i1, v1 ) in strs.enumerated() {
-//         if i1 == 0 {
-//             answer.append(-1)
-//             continue
-//         }
-        
-//         var slicedArray = Array(strs[0...i1-1]).reversed()
-        
-//         if slicedArray.contains(v1) {
-//             for ( i2, v2) in slicedArray.enumerated() {
-//                 if v1 == v2 {
-//                     answer.append(i2 + 1)
-//                     break
-//                 }
-//             }
-//         } else {
-//             answer.append(-1)
-//         }  
-//     }
     
     return answer
 }

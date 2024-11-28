02540065aa06377916d17e5374c055006dbd39b5
Seongto <imseongto@gmail.com>
2024-11-28 09:58:04 +0900

    [level 1] Title: 기사단원의 무기, Time: 45.10 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.swift
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.swift
@@ -1,51 +1,20 @@
 import Foundation
 
 func solution(_ number:Int, _ limit:Int, _ power:Int) -> Int {
-    var answer: Int = 0
-    var soldiers: Set<Int> = Set(1...number)
-    
-    for num in 1...number {
-        if !soldiers.contains(num) {
-            continue
-        }
-        
-        var divCount: Int = 0
-        var isOver: Bool = false
-        
-        
-        var i = 1
-        while i * i <= num {
-            if i * i == num {
-                divCount += 1
-            } else if num % i == 0 {
-                divCount += 2
-            }
-            
-            if divCount > limit {
-                isOver = true
-                break
-            }
-            
-            i += 1
-        }
-        
-        if isOver {
-            answer += power
-        } else {
-            answer += divCount
-        }
+    var answer = [Int]( repeating: 0, count: number + 1 )
 
-        if ( isOver == true ) || ( divCount == limit ) {
-            if number / num > 1 {
-                for mul in 2...number / num {
-                    if soldiers.contains(mul * num){
-                        soldiers.remove(mul * num)
-                        answer += power
-                    }
-                }
+        for num in 1...number {
+            var current = num
+
+            while current <= number {
+                answer[current] += 1
+                current += num
             }
+        
+            // print("num: \(num), \(answer)")
         }
-    }
     
-    return answer
+    answer = answer.map { $0 > limit ? power : $0 }
+    
+    return answer.reduce(0, +)
 }

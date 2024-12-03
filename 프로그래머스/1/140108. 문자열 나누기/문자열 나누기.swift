b715a5374bb8cff69c76959dd5e3934bf4efb4c8
Seongto <imseongto@gmail.com>
2024-12-03 15:47:11 +0900

    [level 1] Title: 문자열 나누기, Time: 1533.88 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.swift
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.swift
@@ -3,8 +3,7 @@ import Foundation
 func solution(_ s:String) -> Int {
     var first: Character = " "
     var isFirst: Bool = true
-    var amount1: Int = 0
-    var amount2: Int = 0
+    var count: Int = 0
     var answer:Int = 0
     
     s.enumerated().forEach {
@@ -14,18 +13,17 @@ func solution(_ s:String) -> Int {
         }
         
         if $0.1 == first {
-            amount1 += 1
+            count += 1
         } else {
-            amount2 += 1
+            count -= 1
         }
         
         if $0.0 == s.count - 1 {
             answer += 1
-        } else if amount1 == amount2 {
+        } else if count == 0 {
             answer += 1
             isFirst = true
-            amount1 = 0
-            amount2 = 0
+            count = 0
         }
     }
     

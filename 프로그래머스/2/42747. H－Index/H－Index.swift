8cb8de00f36a9bfa4ee19e25f8a6286db844e7c4
Seongto <imseongto@gmail.com>
2024-12-26 23:49:36 +0900

    [level 2] Title: H-Index, Time: 3.47 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.swift
+++ 프로그래머스/2/42747. H－Index/H－Index.swift
@@ -1,37 +1,16 @@
 import Foundation
 
 func solution(_ citations:[Int]) -> Int {
-    var sorted: [Int] = citations.sorted()
-    var minNum: Int = sorted.first!
-    var maxNum: Int = sorted.last!
-    var lastCheckedIdx: Int = 0
-    
-    if sorted[0] >= sorted.count {
-        return sorted.count
-    }
-    
-    var answer: Int = 0
-    
-    print(sorted)
-        
-    for num in minNum...maxNum {
-        for i in lastCheckedIdx..<sorted.count {
-            if sorted[i] <= num {
-                lastCheckedIdx = i
-            } else {
-                break
-            }
-        }
-        
-        print("lastIdx : \(lastCheckedIdx), num : \(num)")
-        
-        var count: Int = sorted.count - lastCheckedIdx
-        if num < count {
-            answer = num
+    let sorted = citations.sorted(by: >)
+    var result = 0
+
+    for i in 0..<sorted.count {
+        if i + 1 <= sorted[i] {
+            result = i + 1
         } else {
             break
         }
     }
-    
-    return answer
+
+    return result
 }
